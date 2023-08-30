3b97fdd01be64cd7c5d5b603a41bcaab0f3145f6
ryosukekameda <138686682+ryosukekameda@users.noreply.github.com>
2023-08-30 18:58:07 +0000

    08310358

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  namespace :admin do
+    get 'admin/top' => 'admin#top', as: 'admin'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+  end
+  
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  scope module: :public do
+    get 'items' => 'items#index'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
