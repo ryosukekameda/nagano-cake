dfd543ee6e3654404f6b71e260e9171d76c55e3c
ryosukekameda <138686682+ryosukekameda@users.noreply.github.com>
2023-08-31 18:09:23 +0000

    09010309

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   }
   
   namespace :admin do
-    get 'admin/top' => 'admin#top', as: 'admin'
+    get '/admin' => 'admin#top', as: 'admin'
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show]
   end
   
   devise_for :customers, skip: [:passwords], controllers: {
@@ -14,7 +16,18 @@ Rails.application.routes.draw do
   }
   
   scope module: :public do
-    get 'items' => 'items#index'
+    root to: 'homes#top'
+    get 'homes/about' => 'homes#about', as: 'about'
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:edit, :update]
+    get 'customers/mypage' => 'customers#mypage'
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+    patch 'customers/withdraw' => 'customers#withdraw'
+    resources :cart_items, only: [:index, :update, :create, :destroy]
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+    resources :orders, only: [:new, :index, :create, :show]
+    post 'orders/confirm' => 'orders#confirm'
+    get 'orders/complete' => 'orders#complete'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
