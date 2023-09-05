7f4351c8468858581b8ca2d8adfebfc3591db2f0
ryosukekameda <138686682+ryosukekameda@users.noreply.github.com>
2023-09-05 10:37:20 +0000

    09051937

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,10 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+  has_many :addresses, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
+  has_many :orders, dependent: :destroy
+         
   def full_name
     self.last_name + " " + self.first_name
   end
@@ -11,4 +15,18 @@ class Customer < ApplicationRecord
   def full_name_kana
     self.last_name_kana + " " + self.first_name_kana
   end
+  
+  def customer_status
+    if is_deleted == true
+      "退会"
+    else
+      "有効"
+    end
+  end
+  
+  def active_for_authentication?
+    super && (is_deleted == false)
+  end
 end
+
+
