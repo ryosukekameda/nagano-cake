4cad33306d61d15e201e18e9845af8c74ff67a46
ryosukekameda <138686682+ryosukekameda@users.noreply.github.com>
2023-09-15 18:01:19 +0000

    09160301

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -17,14 +17,23 @@ class Public::CartItemsController < ApplicationController
     if @cart_item.save
        redirect_to cart_items_path
     else
-       render
+      flash[:alert] = "個数を選択してください"
+       render item_path(@item.id)
     end
   end
   
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    flash.now[:alert] = "#{@cart_item.item.name}を削除しました"
+    redirect_to cart_items_path
   end
   
   def destroy_all
+    @cart_item = current_customer.cart_items
+    @cart_item.destroy_all
+    flash[:alert] = "カート内の商品を全て削除しました"
+    redirect_to cart_items_path
   end
   
   private
