73f5e92d8531cdac7dc20c5688bb5b6a6edda18c
ryosukekameda <138686682+ryosukekameda@users.noreply.github.com>
2023-09-16 19:24:22 +0000

    09170424

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -19,8 +19,39 @@ class Public::OrdersController < ApplicationController
   end
   
   def complete
+  　order = Order.new(session[:order])
+  　order.save
+  　cart_items = current_customer.cart_items
+  　cart_items.each do |cart_item|
+  　   order_detail = OrderDetail.new
+  　   order_detail.order_id = order.id
+  　   order_detai.amount = cart_item.amount
+  　   order_detail.making_status = 0
+  　   order_detail.save
   end
   
   def create
+    customer = current_customer
+    session[:order] = Order.new
+    cart_items = current_customer.cart_items
+    sum = 0
+    cart_items.each do |cart_item|
+      sum += (cart_item.item.price_without_tax * 1.1).floor * cart_item.amount
+    end
+    session[:order][:postage] = 800
+    session[:order][:billing_amount] = sum + session[:order][:postage]
+    session[:order][:order_status] = 0
+    session[:order][:customer_id] = current_customer.id
+    session[:order][:billing_amount] = params[:method].to_i
+    session[:order][:post_cade] = params[:post_code]
+    session[:order][:address] = params[:address]
+    session[:order][:name] = params[:name]
+    if session[:order][:post_code].presence && session[:order][:address].presence && session[:order][:name].presence
+       redirect_to confirm_path
+    else
+       redirect_to new_order_path
+    end
+  end
+  
   end
 end
