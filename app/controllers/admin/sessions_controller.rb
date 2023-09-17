8e7cb82d3ff02dd6bea22008c120e904f1a9e259
ryosukekameda <138686682+ryosukekameda@users.noreply.github.com>
2023-09-17 16:12:43 +0000

    09180112

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -19,10 +19,6 @@ class Admin::SessionsController < Devise::SessionsController
 
   # protected
   protected
- 
-    def after_sign_in_path_for(resource)
-      admin_items_path
-    end
 
     def after_sign_out_path_for(resource)
       new_admin_session_path
