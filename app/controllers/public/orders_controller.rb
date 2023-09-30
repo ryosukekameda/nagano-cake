d5f6f905b5afafb9ed203209e1e94addfc2fdd78
ryosukekameda <138686682+ryosukekameda@users.noreply.github.com>
2023-09-30 16:49:39 +0000

    10010149

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -11,54 +11,55 @@ class Public::OrdersController < ApplicationController
   end
   
   def show
-    if params[:id] == "confirm"
-      @order = Order.new(session[:order])
-      @order.created_at = Time.now
+    　@order = Order.find(params[:id])
+    # if params[:id] == "confirm"
+      # @order = Order.new(session[:order])
+      # @order.created_at = Time.now
       @order_details = @order.order_details
-    else
-      @order = Order.new(session[:order])
-      @order_details = @order.order_details
-  　end
+    # else
+      # @order = Order.new(session[:order])
+      # @order.created_at = Time.now
+      # @order_details = @order.order_details
+    # end
   end
-
   
   def confirm
-    customer = current_customer
-    @order = Order.new(session[:order])
     @cart_items = current_customer.cart_items
-    sum = 0
-    @cart_items.each do |cart_item|
-      sum += (cart_item.item.price_without_tax * 1.1).floor * cart_item.amount
-    end
-    session[:order] = Order.new()
-    session[:order][:postage] = 800
-    session[:order][:billing_amount] = sum + session[:order][:postage]
-    session[:order][:payment_method] = params[:method].to_i
-    session[:order][:postal_code] = customer.postal_code
-    session[:order][:address] = customer.address
-    session[:order][:name] = customer.full_name
+    # customer = current_customer
+    # @order = Order.new(session[:order])
+    # sum = 0
+    # @cart_items.each do |cart_item|
+      # sum += (cart_item.item.price_without_tax * 1.1).floor * cart_item.amount
+    # end
+    # session[:order] = Order.new()
+    # session[:order][:postage] = 800
+    # session[:order][:billing_amount] = sum + session[:order][:postage]
+    # session[:order][:payment_method] = params[:method].to_i
+    # session[:order][:postal_code] = customer.postal_code
+    # session[:order][:address] = customer.address
+    # session[:order][:name] = customer.full_name
   end
   
   def complete
     @order = Order.new(order_params)
     @order.customer_id = current_customer.id
     @order.save
-     cart_items = current_customer.cart_items
-     cart_items.each do |cart_item|
-       order_detail = OrderDetail.new
-       order_detail.order_id = @order.id
-       order_detail.amount = cart_item.amount
-       order_detail.making_status = 0
-       order_detail.price = (cart_item.item.price_without_tax * 1.1).floor
-       order_detail.save
-     end
+    cart_items = current_customer.cart_items
+    cart_items.each do |cart_item|
+    order_detail = OrderDetail.new
+    order_detail.order_id = @order.id
+    order_detail.amount = cart_item.amount
+    order_detail.making_status = 0
+    order_detail.price = (cart_item.item.price_without_tax * 1.1).floor
+    order_detail.save
+    end
       cart_items.destroy_all
-      redirect_to complete_order_path(@order)
+      # redirect_to complete_order_path(@order)
   end
   
   def create
     customer = current_customer
-    session[:order] = {}
+    session[:order] = Order.new
     cart_items = current_customer.cart_items
     sum = 0
     cart_items.each do |cart_item|
@@ -74,15 +75,16 @@ class Public::OrdersController < ApplicationController
       session[:order][:address] = customer.address
       session[:order][:name] = customer.full_name
     if session[:order][:postal_code].presence && session[:order][:address].presence && session[:order][:name].presence
-       redirect_to confirm_path(id: "confirm")
+       redirect_to confirm_path
     else
        redirect_to new_order_path
     end
   end
   
-private
+  private
 
   def order_params
     params.require(:order).permit(:customer_id, :postage, :billing_amount, :payment_method, :ordr_status, :post_code, :address, :name)
   end
 end
+
